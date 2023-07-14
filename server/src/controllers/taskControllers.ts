89b871b7424558164aeec54a6215de27c630e1ad
Victor Guerrero <vicguerrero.dev@gmail.com>
2023-07-14 14:55:24 -0400

    Build DELETE request to Task functionality

--- server/src/controllers/taskControllers.ts
+++ server/src/controllers/taskControllers.ts
@@ -5,7 +5,7 @@ const getAllTasks = async (req: Request, res: Response) => {
   try {
     const task = new Task();
     const tasks = await task.getTasks();
-    res.json(tasks.rows);
+    res.status(200).json(tasks.rows);
   } catch (error: unknown) {
     if (error instanceof Error) {
       res.status(500).json({ error: "An error occurred" });
@@ -17,16 +17,27 @@ const getAllTasks = async (req: Request, res: Response) => {
 const postTask = async (req: Request, res: Response) => {
   try {
     const task = new Task();
-    const result = await task.addTask(req.body);
-    console.log("Item has been added: " + result);
+    await task.addTask(req.body);
+    res.status(201).json({ message: "Item has been added" });
   } catch (error: unknown) {
     if (error instanceof Error) {
-      console.log("Oh no!!");
-      console.log(req.body);
       res.status(500).json({ error: "An error occurred" });
       console.error(error.message);
     }
   }
 };
 
-export { getAllTasks, postTask };
+const deleteTask = async (req: Request, res: Response) => {
+  try {
+    const task = new Task();
+    await task.deleteTask(Number(req.params.id));
+    res.status(200).json({ message: "Item has been deleted" });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(500).json({ error: "An error occurred" });
+      console.error(error.message);
+    }
+  }
+};
+
+export { getAllTasks, postTask, deleteTask };
--- server/src/routes/taskRoutes.ts
+++ server/src/routes/taskRoutes.ts
@@ -1,10 +1,16 @@
 import express, { Router } from "express";
-import { getAllTasks, postTask } from "../controllers/taskControllers";
+import {
+  getAllTasks,
+  postTask,
+  deleteTask,
+} from "../controllers/taskControllers";
 
 const router = express.Router();
 
 router.get("/", getAllTasks);
 
 router.post("/", postTask);
+
+router.delete("/:id", deleteTask);
 
 export default router;
